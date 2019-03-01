3048a340109e789edbe4e3ae95c0919017e75cc6
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-03-01 11:22:29 -0300

    feat(database-setup): add migrations setup

--- revert/initial_schema.sql
+++ revert/initial_schema.sql
@@ -1,5 +1,15 @@
 BEGIN;
 
-DROP SCHEMA initial_schema;
+drop table members;
+
+drop table projects;
+
+drop table organizations;
+
+drop table messages;
+
+drop table notifications;
+
+drop table app_users;
 
 COMMIT;
--- verify/initial_schema.sql
+++ verify/initial_schema.sql
@@ -1,5 +1,15 @@
 BEGIN;
 
-SELECT pg_catalog.has_schema_privilege('initial_schema', 'usage');
+select 1 from members;
+
+select 1 from projects;
+
+select 1 from organizations;
+
+select 1 from messages;
+
+select 1 from notifications;
+
+select 1 from app_users;
 
 ROLLBACK;
