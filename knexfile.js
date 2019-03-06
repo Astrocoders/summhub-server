43946057c7fa8344caa89413616fa7e242d366a8
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-03-06 17:30:13 -0300

    chore(migrations): add knex

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'summhub_dev',
+      user:     process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'summhub_prod',
+      user:     process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
