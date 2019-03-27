926cece2b5e6edec5ffcf7ba44d59849b9f2dfd6
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2019-03-27 20:07:37 -0300

    feat(scripts): add wait_for_postgres

---
+++ wait-for-postgres.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
